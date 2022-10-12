c0008e492fa7299c0a830a2edf7a7dd3c7f9cfa6
sTefbured <stef.for.ever2000@gmail.com>
2022-10-12 03:20:29 +0300

    Separate parser logic from UI creation logic

---
+++ onClickBuilder.js
@@ -0,0 +1,106 @@
+const checkAndSkipNonObjectPart = (isSkipRequired, logs) => {
+    let currentCharIndex = 0;
+    let output = "";
+    if (isSkipRequired) {
+        currentCharIndex = getObjectBodyStartIndex(logs);
+        if (currentCharIndex > 0) {
+            output = logs.substring(0, currentCharIndex) + "\n";
+        }
+    }
+    return [output, currentCharIndex];
+};
+
+const getTabs = (count) => {
+    let result = "";
+    while (count--) {
+        result += "    ";
+    }
+    return result;
+};
+
+const getObjectBodyStartIndex = (logs) => {
+    const PARAMS_STR = "with params";
+    const RESULT_STR = "with result";
+    let paramsIndex = logs.indexOf(PARAMS_STR);
+    paramsIndex = paramsIndex < 0 ? Number.POSITIVE_INFINITY : paramsIndex + PARAMS_STR.length;
+    let resultIndex = logs.indexOf(RESULT_STR);
+    resultIndex = resultIndex < 0 ? Number.POSITIVE_INFINITY : resultIndex + RESULT_STR.length;
+    let result = Math.min(paramsIndex, resultIndex);
+    if (result === Number.POSITIVE_INFINITY) {
+        return 0;
+    }
+    if (logs[result] === ":") {
+        result++;
+    }
+    return result;
+};
+
+export const createOnClick = (inputElement, outputElement, parameterElementsMap) => {
+    return () => {
+        let logs = inputElement.value;
+        if (!logs) {
+            return;
+        }
+        let isSkippingNonObjectBodyPart = parameterElementsMap.get(SKIP_NON_OBJECT_BODY_PART).checked;
+        let [output, currentCharIndex] = checkAndSkipNonObjectPart(isSkippingNonObjectBodyPart, logs);
+        let tabsCount = 0;
+        let tabs = "";
+        let isValue = false;
+        let isInSentence = false;
+        for (currentCharIndex; currentCharIndex < logs.length; currentCharIndex++) {
+            let currentChar = logs[currentCharIndex];
+            switch (currentChar) {
+                case "[":
+                case "{":
+                    isValue = false;
+                    isInSentence = false;
+                    tabs = getTabs(++tabsCount);
+                    output += currentChar + "\n" + tabs;
+                    break;
+                case "]":
+                case "}":
+                    isValue = false;
+                    isInSentence = false;
+                    tabs = getTabs(--tabsCount);
+                    output += "\n" + tabs + currentChar;
+                    let isLastOnLine = true;
+                    for (let i = currentCharIndex + 1; i < logs.length; i++) {
+                        if (["\t", " ", "\n"].includes(logs[i])) {
+                            continue;
+                        }
+                        if (["}", "]", ","].includes(logs[i])) {
+                            isLastOnLine = false;
+                        }
+                        break;
+                    }
+                    if (isLastOnLine) {
+                        output += "\n" + tabs;
+                    }
+                    break;
+                case "=":
+                    isValue = true;
+                    output += " " + currentChar + " ";
+                    break;
+                case ",":
+                    isValue = false;
+                    isInSentence = false;
+                    output += currentChar + "\n" + tabs;
+                    break;
+                case "\t":
+                case " ":
+                case "\n":
+                    if (isValue && isInSentence && !["\t", " ", "\n", ",", "]", "}"].includes(logs[currentCharIndex + 1])) {
+                        output += currentChar;
+                    }
+                    break;
+                default:
+                    if (isValue) {
+                        isInSentence = true;
+                    }
+                    output += currentChar;
+                    break;
+            }
+        }
+        outputElement.value = output;
+    };
+};
--- pageBuilder.js
+++ pageBuilder.js
@@ -1,3 +1,4 @@
+import {createOnClick} from "./onClickBuilder.js";
 {
     const SKIP_NON_OBJECT_BODY_PART = "Skip non-object-body part";
 
@@ -27,117 +28,13 @@
         return checkboxLabel;
     };
 
-    const getTabs = (count) => {
-        let result = "";
-        while (count--) {
-            result += "    ";
-        }
-        return result;
-    };
-
-    const getObjectBodyStartIndex = (logs) => {
-        const PARAMS_STR = "with params";
-        const RESULT_STR = "with result";
-        let paramsIndex = logs.indexOf(PARAMS_STR);
-        paramsIndex = paramsIndex < 0 ? Number.POSITIVE_INFINITY : paramsIndex + PARAMS_STR.length;
-        let resultIndex = logs.indexOf(RESULT_STR);
-        resultIndex = resultIndex < 0 ? Number.POSITIVE_INFINITY : resultIndex + RESULT_STR.length;
-        let result = Math.min(paramsIndex, resultIndex);
-        if (result === Number.POSITIVE_INFINITY) {
-            return 0;
-        }
-        if (logs[result] === ":") {
-            result++;
-        }
-        return result;
-    };
-
     const createGetResultButton = (inputElement, outputElement, parameterElementsMap) => {
         let button = document.createElement("button");
         button.textContent = "Get result";
         button.style.fontSize = "20px";
         button.style.height = "50px";
         button.style.cursor = "pointer";
-        button.onclick = () => {
-            let logs = inputElement.value;
-            if (!logs) {
-                return;
-            }
-            let isSkippingNonObjectBodyPart = parameterElementsMap.get(SKIP_NON_OBJECT_BODY_PART).checked;
-            let currentCharIndex = 0;
-            let output = "";
-            if (isSkippingNonObjectBodyPart) {
-                currentCharIndex = getObjectBodyStartIndex(logs);
-                if (currentCharIndex > 0) {
-                    output = logs.substring(0, currentCharIndex) + "\n";
-                }
-            }
-            let tabsCount = 0;
-            let tabs = "";
-            let isValue = false;
-            let isInSentence = false;
-            for (currentCharIndex; currentCharIndex < logs.length; currentCharIndex++) {
-                let currentChar = logs[currentCharIndex];
-                switch (currentChar) {
-                    case "[":
-                    case "{":
-                        isValue = false;
-                        isInSentence = false;
-                        tabs = getTabs(++tabsCount);
-                        output += currentChar + "\n" + tabs;
-                        break;
-                    case "]":
-                    case "}":
-                        isValue = false;
-                        isInSentence = false;
-                        tabs = getTabs(--tabsCount);
-                        output += "\n" + tabs + currentChar;
-                        let isLastOnLine = true;
-                        for (let i = currentCharIndex + 1; i < logs.length; i++) {
-                            if (["\t", " ", "\n"].includes(logs[i])) {
-                                continue;
-                            }
-                            if (["}", "]", ","].includes(logs[i])) {
-                                isLastOnLine = false;
-                            }
-                            break;
-                        }
-                        if (isLastOnLine) {
-                            output += "\n" + tabs;
-                        }
-                        break;
-                    case "=":
-                        isValue = true;
-                        output += " " + currentChar + " ";
-                        break;
-                    case ",":
-                        isValue = false;
-                        isInSentence = false;
-                        output += currentChar + "\n" + tabs;
-                        break;
-                    case "\t":
-                    case " ":
-                    case "\n":
-                        if (
-                            isValue &&
-                            isInSentence &&
-                            !["\t", " ", "\n", ",", "]", "}"].includes(
-                                logs[currentCharIndex + 1]
-                            )
-                        ) {
-                            output += currentChar;
-                        }
-                        break;
-                    default:
-                        if (isValue) {
-                            isInSentence = true;
-                        }
-                        output += currentChar;
-                        break;
-                }
-            }
-            outputElement.value = output;
-        };
+        button.onclick = createOnClick(inputElement, outputElement, parameterElementsMap);
         return button;
     };
 
